dada1c47426c9719097586449c0b7f21c0fedf40
sanchitm25 <sanchit550s.m@gmail.com>
2024-04-26 03:41:11 -0400

    .

--- src/Panel2048.java
+++ src/Panel2048.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Image;
 import java.awt.GridBagLayout;
 import java.io.File;
 import java.io.IOException;
@@ -13,6 +14,7 @@ import javax.swing.JPanel;
 public class Panel2048 extends JPanel {
     private Square[][] blocks = new Square[4][4];
     private JLabel background;
+    private boolean continuePlay = true;
 
     public Panel2048() {
         super();
@@ -28,7 +30,7 @@ public class Panel2048 extends JPanel {
         KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new KeyEventDispatcher() {
             @Override
             public boolean dispatchKeyEvent(KeyEvent e) {
-                if (KeyEvent.KEY_PRESSED == e.getID()) {
+                if (KeyEvent.KEY_PRESSED == e.getID() && continuePlay) {
                     switch (e.getKeyCode()) {
                         case 37: // Left
                             moveLeft();
@@ -46,6 +48,9 @@ public class Panel2048 extends JPanel {
                             moveDown();
                             generateSquare();
                             break;
+                        case 10: // Enter
+                            generateSquare();
+                            break;
                     }
                 }
                 return false;
@@ -56,9 +61,9 @@ public class Panel2048 extends JPanel {
     private void moveLeft() {
         clearGrid();
     
-        for (int j = 0; j < 4; j++) { // j = row
-            for (int k = 0; k < 4; k++) {
-                for (int i = 0; i < 3; i++) { // i = column
+        for (int j = 0; j < 4; j++) {   // j = row
+            for (int k = 0; k < 4; k++) {   // iterations to merge all squares (redundant)
+                for (int i = 0; i < 3; i++) {   // i = column
                     if (blocks[i][j] != null) {
                         blocks[i+1][j] = blocks[i][j].mergeSquares(blocks[i+1][j]);
                     } else {
@@ -67,41 +72,20 @@ public class Panel2048 extends JPanel {
                     }
                 }
             }
-            // for (int i = 0; i < 3; i++) { // i = column
-            //     if (blocks[i][j] != null) {
-            //         blocks[i+1][j] = blocks[i][j].mergeSquares(blocks[i+1][j]);
-            //     } else {
-            //         blocks[i][j] = blocks[i+1][j];
-            //         blocks[i+1][j] = null;
-            //     }
-            // }
-    
-            // for (int i = 0; i < 2; i++) { // i = column
-            //     if (blocks[i][j] != null) {
-            //         blocks[i+1][j] = blocks[i][j].mergeSquares(blocks[i+1][j]);
-            //     } else {
-            //         blocks[i][j] = blocks[i+1][j];
-            //         blocks[i+1][j] = null;
-            //     }
-            // }
-    
-            // if (blocks[0][j] != null) {
-            //     blocks[1][j] = blocks[0][j].mergeSquares(blocks[1][j]);
-            // } else {
-            //     blocks[0][j] = blocks[1][j];
-            //     blocks[1][j] = null;
-            // }
         }
     
         updateGrid();
+
+        checkForWin();
+        checkForLose();
     }
 
     private void moveUp() {
         clearGrid();
 
-        for (int i = 0; i < 4; i++) { // i = column
-            for (int k = 0; k < 4; k++) {
-                for (int j = 0; j < 3; j++) { // j = row
+        for (int i = 0; i < 4; i++) {   // i = column
+            for (int k = 0; k < 4; k++) {   // iterations to merge all squares (redundant)
+                for (int j = 0; j < 3; j++) {   // j = row
                     if (blocks[i][j] != null) {
                         blocks[i][j+1] = blocks[i][j].mergeSquares(blocks[i][j+1]);
                     } else {
@@ -110,41 +94,20 @@ public class Panel2048 extends JPanel {
                     }
                 }
             }
-            // for (int j = 0; j < 3; j++) { // j = row
-            //     if (blocks[i][j] != null) {
-            //         blocks[i][j+1] = blocks[i][j].mergeSquares(blocks[i][j+1]);
-            //     } else {
-            //         blocks[i][j] = blocks[i][j+1];
-            //         blocks[i][j+1] = null;
-            //     }
-            // }
-
-            // for (int j = 0; j < 2; j++) { // j = row
-            //     if (blocks[i][j] != null) {
-            //         blocks[i][j+1] = blocks[i][j].mergeSquares(blocks[i][j+1]);
-            //     } else {
-            //         blocks[i][j] = blocks[i][j+1];
-            //         blocks[i][j+1] = null;
-            //     }
-            // }
-
-            // if (blocks[i][0] != null) {
-            //     blocks[i][1] = blocks[i][0].mergeSquares(blocks[i][1]);
-            // } else {
-            //     blocks[i][0] = blocks[i][1];
-            //     blocks[i][1] = null;
-            // }
         }
 
         updateGrid();
+
+        checkForWin();
+        checkForLose();
     }
 
     private void moveRight() {
         clearGrid();
     
-        for (int j = 0; j < 4; j++) { // j = row
-            for (int k = 0; k < 3; k++) {
-                for (int i = 3; i > 0; i--) { // i = column, from right to left
+        for (int j = 0; j < 4; j++) {   // j = row
+            for (int k = 0; k < 3; k++) {   // iterations to merge all squares (redundant)
+                for (int i = 3; i > 0; i--) {   // i = column, from right to left
                     if (blocks[i][j] != null) {
                         blocks[i-1][j] = blocks[i][j].mergeSquares(blocks[i-1][j]);
                     } else {
@@ -153,42 +116,20 @@ public class Panel2048 extends JPanel {
                     }
                 }
             }
-
-            // for (int i = 3; i > 0; i--) { // i = column, from right to left
-            //     if (blocks[i][j] != null) {
-            //         blocks[i-1][j] = blocks[i][j].mergeSquares(blocks[i-1][j]);
-            //     } else {
-            //         blocks[i][j] = blocks[i-1][j];
-            //         blocks[i-1][j] = null;
-            //     }
-            // }
-    
-            // for (int i = 3; i > 1; i--) { // i = column, from right to left
-            //     if (blocks[i][j] != null) {
-            //         blocks[i-1][j] = blocks[i][j].mergeSquares(blocks[i-1][j]);
-            //     } else {
-            //         blocks[i][j] = blocks[i-1][j];
-            //         blocks[i-1][j] = null;
-            //     }
-            // }
-    
-            // if (blocks[3][j] != null) {
-            //     blocks[2][j] = blocks[3][j].mergeSquares(blocks[2][j]);
-            // } else {
-            //     blocks[3][j] = blocks[2][j];
-            //     blocks[2][j] = null;
-            // }
         }
     
         updateGrid();
+
+        checkForWin();
+        checkForLose();
     }
 
     private void moveDown() {
         clearGrid();
     
-        for (int i = 0; i < 4; i++) { // i = column
-            for (int k = 0; k < 4; k++) {
-                for (int j = 3; j > 0; j--) { // j = row
+        for (int i = 0; i < 4; i++) {   // i = column
+            for (int k = 0; k < 4; k++) {   // iterations to merge all squares (redundant)
+                for (int j = 3; j > 0; j--) {   // j = row
                     if (blocks[i][j] != null) {
                         blocks[i][j-1] = blocks[i][j].mergeSquares(blocks[i][j-1]);
                     } else {
@@ -197,34 +138,53 @@ public class Panel2048 extends JPanel {
                     }
                 }
             }
-
-            // for (int j = 3; j > 0; j--) { // j = row
-            //     if (blocks[i][j] != null) {
-            //         blocks[i][j-1] = blocks[i][j].mergeSquares(blocks[i][j-1]);
-            //     } else {
-            //         blocks[i][j] = blocks[i][j-1];
-            //         blocks[i][j-1] = null;
-            //     }
-            // }
-    
-            // for (int j = 3; j > 1; j--) { // j = row
-            //     if (blocks[i][j] != null) {
-            //         blocks[i][j-1] = blocks[i][j].mergeSquares(blocks[i][j-1]);
-            //     } else {
-            //         blocks[i][j] = blocks[i][j-1];
-            //         blocks[i][j-1] = null;
-            //     }
-            // }
-    
-            // if (blocks[i][3] != null) {
-            //     blocks[i][2] = blocks[i][3].mergeSquares(blocks[i][2]);
-            // } else {
-            //     blocks[i][3] = blocks[i][2];
-            //     blocks[i][2] = null;
-            // }
         }
     
         updateGrid();
+
+        checkForWin();
+        checkForLose();
+    }
+
+    private void checkForWin() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (blocks[i][j] != null && blocks[i][j].getValue() == 2048) {
+                    JLabel winMessage = new JLabel();
+                    try {
+                        Image image = ImageIO.read(new File("pictures/win.png"));
+                        winMessage.setIcon(new javax.swing.ImageIcon(image));
+                        winMessage.setBounds(25, 65, 300, 250);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    continuePlay = false;
+                    break;
+                }
+            }
+        }
+    }
+
+    private void checkForLose() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (blocks[i][j] == null) {
+                    return;
+                }
+            }
+        }
+
+        JLabel loseMessage = new JLabel();
+        try {
+            Image image = ImageIO.read(new File("pictures/lose.png"));
+            loseMessage.setIcon(new javax.swing.ImageIcon(image));
+            loseMessage.setBounds(25, 65, 300, 250);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        continuePlay = false;
     }
 
     private void generateSquare() {
@@ -232,7 +192,7 @@ public class Panel2048 extends JPanel {
             int rand1 = (int) (Math.random() * 4);
             int rand2 = (int) (Math.random() * 4);
 
-            if (blocks[rand1][rand2] == null) {
+            if (blocks[rand1][rand2] == null) { // Fix infinite loop issue
                 Square square = new Square(2);
                 blocks[rand1][rand2] = square;
                 background.add(blocks[rand1][rand2]);
@@ -248,6 +208,7 @@ public class Panel2048 extends JPanel {
             for (int j = 0; j < 4; j++) {
                 if (blocks[i][j] != null) {
                     background.remove(blocks[i][j]);
+                    background.repaint();
                 }
             }
         }
@@ -262,22 +223,22 @@ public class Panel2048 extends JPanel {
                 }
             }
         }
-        printGrid();
+        //printGrid();
     }
 
-    private void printGrid() {
-        for (int j = 0; j < 4; j++) {
-            for (int i = 0; i < 4; i++) {
-                if (blocks[i][j] != null) {
-                    System.out.print(blocks[i][j].toString());
-                } else {
-                    System.out.print("0");
-                }
-            }
-            System.out.print("\n");
-        }
-        System.out.print("\n");
-    }
+    // private void printGrid() {
+    //     for (int j = 0; j < 4; j++) {
+    //         for (int i = 0; i < 4; i++) {
+    //             if (blocks[i][j] != null) {
+    //                 System.out.print(blocks[i][j].toString());
+    //             } else {
+    //                 System.out.print("0");
+    //             }
+    //         }
+    //         System.out.print("\n");
+    //     }
+    //     System.out.print("\n");
+    // }
 
     private static JLabel background() {
         JLabel background = new JLabel();
